b13897564da5a123a24688361730d7092fd94ba7
Allen Rhee <rhee.daehyun@gmail.com>
2018-09-07 12:57:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('div#nested div.target');
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll("#app .ranked-list li");
+  
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML) + n).toString();
+  }
+  return lis;
+} 
+
+function deepestChild() {
+  let d = document.querySelectorAll('#grand-node div');
+  let a = [];
+  
+  for (let i = 0; i < d.length; i++) {
+		a.push(d[i])
+  }
+  return deepestChild()
+  
+//  return document.querySelector('#grand-node div div div div');
+}
